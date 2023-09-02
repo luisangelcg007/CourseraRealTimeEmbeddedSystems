55b23ddf8de5d0d8c6e9d90f34634bbb103807c1
luisangelcg007 <luisangelcg007@gmail.com>
2023-09-01 23:55:18 -0600

    threads ready!

--- 1_RealTimeEmbeddedSystemsConceptsAndPractices/Assignment1HelloWorldSimpleThreadCreation/simplethread/source/pthread.c
+++ 1_RealTimeEmbeddedSystemsConceptsAndPractices/Assignment1HelloWorldSimpleThreadCreation/simplethread/source/pthread.c
@@ -2,65 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sched.h>
-
-
-#include "globalDefine.h"
-
-//#define NUM_THREADS 12
-
-/*typedef struct
-{
-    int threadIdx;
-} threadParams_t;*/
-
-
-// POSIX thread declarations and scheduling attributes
-//
-pthread_t threads[NUM_THREADS];
-threadParams_t threadParams[NUM_THREADS];
-
-
-void *counterThread(void *threadp)
-{
-    int sum=0, i;
-    threadParams_t *threadParams = (threadParams_t *)threadp;
-
-    for(i=1; i < (threadParams->threadIdx)+1; i++)
-        sum=sum+i;
- 
-    printf("Thread idx=%d, sum[0...%d]=%d\n", 
-           threadParams->threadIdx,
-           threadParams->threadIdx, sum);
-}
-
-
-int main (int argc, char *argv[])
-{
-   int rc;
-   int i;
-
-   for(i=0; i < NUM_THREADS; i++)
-   {
-       threadParams[i].threadIdx=i;
-
-       pthread_create(&threads[i],   // pointer to thread descriptor
-                      (void *)0,     // use default attributes
-                      counterThread, // thread function entry point
-                      (void *)&(threadParams[i]) // parameters to pass in
-                     );
-
-   }
-
-   for(i=0;i<NUM_THREADS;i++)
-       pthread_join(threads[i], NULL);
-
-   printf("TEST COMPLETE\n");
-}
-/*
-#include <pthread.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sched.h>
 #include <syslog.h>
 #include <sys/utsname.h>
 #include "globalDefine.h"
@@ -68,4 +9,42 @@ int main (int argc, char *argv[])
 // POSIX thread declarations and scheduling attributes
 pthread_t threads[NUM_THREADS];
 threadParams_t threadParams[NUM_THREADS];
+
+/**
+ * @brief Prints a "Hello World from Thread" message.
+ * @param threadp A pointer to a threadParams_t structure that contains the thread index.
+ * @return void.
 */
+void *printMessageThread(void *threadp)
+{
+    openlog("pthread", LOG_PID|LOG_CONS, LOG_USER);
+    syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT:1] Hello World from Thread!");
+    closelog();
+}
+
+/**
+ * @brief The main function of the program.
+*/
+int main (int argc, char *argv[])
+{    struct utsname unameData;
+
+    openlog("pthread", LOG_PID|LOG_CONS, LOG_USER);
+    uname(&unameData);
+    syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT:1] %s", unameData.sysname);
+    closelog();
+
+    for(int i = 0; i < NUM_THREADS; i++)
+    {
+        pthread_t thread;
+        threadParams_t threadParams;
+        threadParams.threadIdx=i;
+
+        pthread_create(&thread, NULL, printMessageThread, (void *)&threadParams);
+    }
+
+    openlog("pthread", LOG_PID|LOG_CONS, LOG_USER);
+    syslog(LOG_INFO, "[COURSE:1][ASSIGNMENT:1] Hello World from Main!");
+    closelog();
+
+    return 0;
+}
